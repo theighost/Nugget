f1e0d9f896c66e0194538058d5f253e4e62f2c66
Sharaf Abusereya <Sharaf@192-168-0-100.rdsnet.ro>
2019-04-07 16:00:02 +0300

    First

--- scripts/facebook.js
+++ scripts/facebook.js
@@ -2,13 +2,13 @@ const User = require('hubot').User;
 const TextMessage = require('hubot').TextMessage;
 module.exports = function(robot) {
       
-  //Listen for Facebook webhook subscription verification
+  /*Listen for Facebook webhook subscription verification*/
   robot.router.get('/chatBot', function(req, res) {
     return res.send(200, req.query['hub.challenge']);
-    //return robot.emit('hi', 'req.query.room');
+    /*return robot.emit('hi', 'req.query.room');*/
   });
 
-  //Listen for messages from Facebook messenger
+  /*Listen for messages from Facebook messenger*/
   robot.router.post('/chatBot', function(req, res) {
    const messageEvent = req.body.entry[0].messaging[0];
     
